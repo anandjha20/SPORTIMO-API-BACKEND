950fce0a8b126cd5629605ffc9562edc007fb687
Sumit Hariyani <sumit.hariyani1@gmail.com>
2022-11-14 13:58:52 +0530

    Update config.js
    url update

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 const local_default = require('./local_default.json')
-//mongoose.connect('mongodb://localhost:27017/football_db');
-//mongoose.connect('mongodb://root:rootadmin@mbc-dev-sportsapp.cluster-ca4ypuzoojjz.us-east-1.docdb.amazonaws.com:27017/sportimo?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',{tlsCAFile: `rds-combined-ca-bundle.pem`});
-mongoose.connect(`mongodb://admin:root@10.8.0.106:3000/sportimo?authSource=admin`);
+ // mongoose.connect('mongodb://localhost:27017/football_db');
+  mongoose.connect('mongodb://admin:root@13.127.63.71:27017/sportimo?authSource=admin');
+ // mongoose.connect('mongodb://admin:root@100.26.5.179:300/sportimo?authSource=admin');
+//mongoose.connect('mongodb://admin:root@3.236.132.255:3000/sportimo?authSource=admin');
+//mongoose.connect(local_default.DB_url);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
